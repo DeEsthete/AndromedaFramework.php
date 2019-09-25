bda7be645a8b10b832e704a37fa216a9f1150cf0
JekaLion <djoni-01@mail.ru>
2019-09-25 21:47:50 +0600

    composer, db

--- index.php
+++ index.php
@@ -1,2 +1,55 @@
 <?php
+//use Doctrine\Common\Collections\ArrayCollection;
+//use Doctrine\Inflector\Inflector;
+//use Moontoast\Math\BigNumber;
 
+use Medoo\Medoo;
+
+include "vendor/autoload.php";
+
+//$collection = new ArrayCollection([1,2,3]);
+//$res = $collection->filter(function ($item){
+//    return $item == 2;
+//});
+//var_dump($res->toArray());
+
+//$numbers = [];
+////echo $number->pow("9999");
+//
+//function fib(BigNumber $index)
+//{
+//    if ($index->isLessThan(2)) {
+//        return $index;
+//    }
+//    $first = (new BigNumber($index))->subtract(2);
+//    $second = (new BigNumber($index))->subtract(1);
+//    $res = fib($first)->add(fib($second));
+//    return $res;
+//}
+//echo fib(new BigNumber('8'));
+
+$cfg = include "database.php";
+$db = new Medoo($cfg);
+//$db->insert("users", [
+//    "username" => "leha",
+//    "name" => "Alexey",
+//    "password" => "123"
+//]);
+
+//$db->update("users", [
+//    "username" => "NELEHA"
+//], [
+//    "id" => "2"
+//]);
+
+$db->delete("users", [
+    "id" => "2"
+]);
+
+$users = $db->select("users", "*");
+foreach ($users as $key => $user){
+    foreach ($user as $k => $item){
+        echo "$k => $item</br>";
+    }
+}
+var_dump($users);
